953468415df3e01c30d004d121d1b22c30ea0fb6
LyricPants66133 <colas.alfredo@icloud.com>
2021-03-13 22:53:19 -0500

    Fixed Mistake

--- override_file_writer.py
+++ override_file_writer.py
@@ -7,10 +7,10 @@ f.close
 
 dockerComposeWeb = requests.get('https://raw.githubusercontent.com/LyricPants66133/Jetson_TSD_fullinstall/web-docker-compose.override.yml')
 f = open('web/docker-compose.override.yml', "w")
-f.write(dockerComposeWeb)
+f.write(dockerComposeWeb.text)
 f.close
 
 dockerComposeMl_api = requests.get('https://raw.githubusercontent.com/LyricPants66133/Jetson_TSD_fullinstall/ml_api-docker-compose.override.yml')
 f = open('ml_api/docker-compose.override.yml', "w")
-f.write(dockerComposeMl_api)
+f.write(dockerComposeMl_api.text)
 f.close
