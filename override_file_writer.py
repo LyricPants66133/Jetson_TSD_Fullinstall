0325e78d2712a4e18b6746204330f2d07ac3cf7b
LyricPants66133 <colas.alfredo@icloud.com>
2021-03-13 23:02:53 -0500

    Fixed URL

--- override_file_writer.py
+++ override_file_writer.py
@@ -1,16 +1,16 @@
 import requests
 
-dockerComposeMain = requests.get('https://raw.githubusercontent.com/LyricPants66133/Jetson_TSD_fullinstall/main-docker-compose.yml')
+dockerComposeMain = requests.get('https://raw.githubusercontent.com/LyricPants66133/Jetson_TSD_Fullinstall/master/docker-composeFiles/main-docker-compose.yml')
 f = open('docker-compose.yml', "w")
 f.write(dockerComposeMain.text)
 f.close
 
-dockerComposeWeb = requests.get('https://raw.githubusercontent.com/LyricPants66133/Jetson_TSD_fullinstall/web-docker-compose.override.yml')
+dockerComposeWeb = requests.get('https://raw.githubusercontent.com/LyricPants66133/Jetson_TSD_Fullinstall/master/docker-composeFiles/web-docker-compose.override.yml')
 f = open('web/docker-compose.override.yml', "w")
 f.write(dockerComposeWeb.text)
 f.close
 
-dockerComposeMl_api = requests.get('https://raw.githubusercontent.com/LyricPants66133/Jetson_TSD_fullinstall/ml_api-docker-compose.override.yml')
+dockerComposeMl_api = requests.get('https://raw.githubusercontent.com/LyricPants66133/Jetson_TSD_Fullinstall/master/docker-composeFiles/ml_api-docker-compose.override.yml')
 f = open('ml_api/docker-compose.override.yml', "w")
 f.write(dockerComposeMl_api.text)
 f.close
