4b96be2d189ff8f7cbe5a33a40478c92e81545b7
Lyric Pants <57442104+LyricPants66133@users.noreply.github.com>
2021-03-17 13:14:08 -0400

    Chanegd web and ml_api edits

--- override_file_writer.py
+++ override_file_writer.py
@@ -6,11 +6,11 @@ f.write(dockerComposeMain.text)
 f.close
 
 dockerComposeWeb = requests.get('https://raw.githubusercontent.com/LyricPants66133/Jetson_TSD_Fullinstall/master/docker-composeFiles/web-docker-compose.override.yml')
-f = open('web/docker-compose.override.yml', "w")
+f = open('web/Dockerfile.yml', "w")
 f.write(dockerComposeWeb.text)
 f.close
 
 dockerComposeMl_api = requests.get('https://raw.githubusercontent.com/LyricPants66133/Jetson_TSD_Fullinstall/master/docker-composeFiles/ml_api-docker-compose.override.yml')
-f = open('ml_api/docker-compose.override.yml', "w")
+f = open('ml_api/Dockerfile', "w")
 f.write(dockerComposeMl_api.text)
 f.close
